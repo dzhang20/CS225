2d4a8a68b0e026e9c2e879fe42c1ee74b3a1b4bd
dzhang55 <dzhang55@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-09-01 04:16:04 +0000

    mp1 submission

---
+++ mp1/rgbapixel.cpp
@@ -0,0 +1,22 @@
+#include "rgbapixel.h"
+
+RGBAPixel::RGBAPixel()
+{
+    alpha = 255;
+    red = 255;
+    green = 255;
+    blue = 255;
+}
+
+RGBAPixel::RGBPixel(uint8_t red, uint8_t green, uint8_t blue)
+{
+	red = red;
+	green = green;
+	blue = blue;
+    alpha =255;
+}
+
+
+
+
+
