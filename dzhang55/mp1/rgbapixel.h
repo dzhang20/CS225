26e5a075d2cb97b9e292ae2036338bd7861815e7
dzhang55 <dzhang55@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-09-01 04:16:04 +0000

    mp1 submission

---
+++ dzhang55/mp1/rgbapixel.h
@@ -0,0 +1,23 @@
+#ifndef RGBAPixel_H
+#define RGBAPixel_H
+#include <cstdint>
+#include <ostream>
+using std::uint8_t;
+class RGBAPixel()
+{
+	public:
+		RGBAPixel();
+		RGBAPixel(uint8_t red,uint8_t green,uint8_t blue)
+		uint8_t red;
+		uint8_t green;
+		uint8_t blue;
+		uint8_t alpha;		
+
+
+}
+
+
+
+
+
+#endif
