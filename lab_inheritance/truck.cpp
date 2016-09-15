794ff7c0228d19176461fbcc660b88353117f205
dzhang55 <dzhang55@574e2964-c1e2-4f07-b934-cb5160de0139>
2016-09-15 01:48:21 +0000

    lab_inheritance

--- lab_inheritance/truck.cpp
+++ lab_inheritance/truck.cpp
@@ -141,3 +141,12 @@ void Truck::clear()
     delete engine;
 }
 
+void Truck::draw(PNG* canvas) const{
+	engine->draw(canvas);
+	cabin->draw(canvas);
+	window->draw(canvas);
+	trailer->draw(canvas);
+	for(int i=0;i<NUM_WHEELS; i++)
+		wheels[i]->draw(canvas);
+}
+
